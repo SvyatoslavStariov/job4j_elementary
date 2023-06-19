a68e98b79773c458892ad0b8d4f1f359790071a8
s.starikov <s.starikov@gaskar.group>
2023-06-19 14:35:19 +0500

    4.3.6. Math.abs [#373616 #423941]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,8 +6,8 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (isDiagonal(x1, y1, x2, y2) && x1 != x2 && y1 != y2) {
-                rsl = rsl == 0 ? Math.abs(x2 - x1) : rsl;
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+                rsl = Math.abs(x2 - x1);
             }
         }
         return rsl;
@@ -15,9 +15,5 @@ public class ChessBoard {
 
     private static boolean isValid(int coordinate) {
         return coordinate >= 0 && coordinate <= 7;
-    }
-
-    private static boolean isDiagonal(int x1, int y1, int x2, int y2) {
-        return (x1 + x2) % 2 == (y1 + y2) % 2;
     }
 }

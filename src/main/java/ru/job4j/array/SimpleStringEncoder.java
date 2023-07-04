6cc6d7dab64f802cdffbc00812c0a0f9c0b7bf85
s.starikov <s.starikov@gaskar.group>
2023-07-05 01:07:46 +0500

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049 #427142]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -6,26 +6,18 @@ public class SimpleStringEncoder {
         char symbol = input.charAt(0);
         int counter = 1;
         for (int i = 1; i < input.length(); i++) {
-            if (symbol != input.charAt(i)) {
+            if (symbol == input.charAt(i)) {
+                counter++;
+            } else {
                 result += symbol;
                 if (counter > 1) {
                     result += counter;
                 }
                 symbol = input.charAt(i);
                 counter = 1;
-            } else {
-                counter++;
             }
         }
-        if (symbol == input.charAt(input.length() - 1)) {
-            result += input.charAt(input.length() - 1);
-            if (counter > 1) {
-            result += counter;
-            }
-        } else {
-            result += symbol;
-            result += input.charAt(input.length() - 1);
-        }
-        return result;
+        result += input.charAt(input.length() - 1);
+        return counter > 1 ? result + counter : result;
     }
 }

e88053a95059c93720f6e32e86ab32e49f8a43b3
s.starikov <s.starikov@gaskar.group>
2023-07-11 00:37:22 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MaxTest {
@@ -28,6 +29,48 @@ public class MaxTest {
         int right = 5;
         int result = Max.max(left, right);
         int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax2Equals3Equals4Then4() {
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        int result = Max.max(a, b, c);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax2Equals3Equals7Then7() {
+        int a = 2;
+        int b = 3;
+        int c = 7;
+        int result = Max.max(a, b, c);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax2Equals3Equals4Equals9Then9() {
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        int d = 9;
+        int result = Max.max(a, b, c, d);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax9Equals3Equals4Equals2Then2() {
+        int a = 9;
+        int b = 3;
+        int c = 4;
+        int d = 2;
+        int result = Max.max(a, b, c, d);
+        int expected = 9;
         assertThat(result).isEqualTo(expected);
     }
 }
